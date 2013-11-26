2d59e6c61f4aabdb928dc043534674ad29d9d1b4
107kaka <splk008@qq.com>
2013-11-26 14:57:18 +0800

    test

---
+++ c/new.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  new.c
+ *
+ *    Description:  for test
+ *
+ *        Version:  1.0
+ *        Created:  2013年11月26日 14时52分38秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  ZHU XU (~_~), splk008@qq.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("test\n");
+	return 0;
+}
